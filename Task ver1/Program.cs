dae13fb2fa718b8073bb3d42b55ee358ed0526cf
Maksim <mirror0791@mail.ru>
2023-10-31 22:28:10 +0300

    Add branch feature

--- Task ver1/Program.cs
+++ Task ver1/Program.cs
@@ -8,3 +8,95 @@
 [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → []*/
+
+Console.Write("Введите размер массива: ");
+string input = Console.ReadLine()!;
+int size = GetArrayIndex(input);
+
+
+int GetArrayIndex(string input)
+{
+    int size = 0;
+    try
+    {
+        size = Convert.ToInt32(input);
+        if (size < 1)
+        {
+            Console.WriteLine("Размер массива должен быть не меньше 1");
+            Console.ReadKey();
+            return 0;
+        }
+
+
+        string[] array = new string[size];
+        string length = "   ";
+
+
+        ArrayFromUser(size);
+        Console.WriteLine();
+        Console.WriteLine($"Заданный массив состоящий из {size} элементов: ");
+        Print(array);
+        Console.WriteLine();
+        Console.WriteLine();
+        ResultArray(array);
+        
+
+
+        string[] ArrayFromUser(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Введите {i + 1} элемент массива: ");
+                array[i] = Console.ReadLine()!;
+            }
+            return array;
+        }
+
+
+        void Print(string[] array)
+        {
+            foreach (string el in array)
+            {
+                Console.Write(el + "\t");
+            }
+
+        }
+
+
+        void ResultArray(string[] array)
+        {
+            int size = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length <= length.Length)
+                {
+                    ++size;
+                }
+            }
+
+            if (size > 0)
+            {
+                Console.WriteLine("Массив с элементами состоящими из 3 или менее символов: ");
+                for (int i = 0; i < size + 1; i++)
+                {
+                    if (array[i].Length <= length.Length)
+                    {
+                        Console.Write($"{array[i]} \t");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("В заданном массиве отсутствуют элементы соответствующие условию");
+            }
+        }
+    }
+
+
+    catch
+    {
+        Console.WriteLine("Ошибка! Не задан размер массива!");
+    }
+    return size;
+}
+Console.ReadKey();
